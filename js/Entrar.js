c8df4c53e66a84e651d2724ccc9fa0cd33e622a6
Richard Luan <richardluan084@gmail.com>
2022-11-26 18:50:47 -0300

    PAginas e funcionalidades do login 100%

---
+++ js/Entrar.js
@@ -0,0 +1,19 @@
+function Entrar_index() {
+    let campo_texto = document.getElementById("campo-email").value
+    if (campo_texto != "") {
+        window.location.href = 'pages/Perfis.html'
+    } else {
+        alert("Preencha o campo de email!")
+    }
+}
+
+function Entrar() {
+    let campo_email = document.getElementById("email-telefone").value
+    let campo_senha = document.getElementById("Senha").value
+
+    if (campo_email != "" && campo_senha != "") {
+        window.location.href = 'pages/Perfis.html'
+    } else {
+        alert("Preencha todos os campos de textos!")
+    }
+}
