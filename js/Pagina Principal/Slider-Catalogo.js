ec96e98d10c589e7a160ed6421e39656e471b13c
Richard Luan <richardluan084@gmail.com>
2022-12-19 17:25:05 -0300

    Mais alguns sliders adicionados

--- js/Pagina Principal/Slider-Catalogo.js
+++ js/Pagina Principal/Slider-Catalogo.js
@@ -117,3 +117,123 @@ controle_fda.forEach((control) => {
         })
     })
 })
+
+// Slider apenas para Series pós apocalipticas - abreviação = spa
+
+let corrente_itens_spa = 0
+
+const controle_spa = document.querySelectorAll('.control-spa')
+const itens_spa = document.querySelectorAll('.item-spa')
+const max_itens_spa = itens_spa.length
+
+controle_spa.forEach((control) => {
+    control.addEventListener("click", () => {
+        const isLeft = control.classList.contains('arrow-left')
+
+        isLeft ? corrente_itens_spa -= 5: corrente_itens_spa += 5
+
+        if (corrente_itens_spa >= max_itens_spa) {
+            corrente_itens_spa = 0
+        }
+
+        if ( corrente_itens_spa < 0) {
+            corrente_itens_spa = max_itens_spa - 1
+        }
+
+        itens_spa[corrente_itens_spa].scrollIntoView({
+            inline: 'start',
+            behavior: "smooth",
+            block: "nearest"
+        })
+    })
+})
+
+// Slider apenas para top 10 Filmes - abreviação - topFilme
+
+let corrente_itens_topFilme = 0
+
+const controle_topFilme = document.querySelectorAll('.control-topFilme')
+const itens_topFilme = document.querySelectorAll('.item-topFilme')
+const max_itens_topFilme = itens_topFilme.length
+
+controle_topFilme.forEach((control) => {
+    control.addEventListener("click", () => {
+        const isLeft = control.classList.contains('arrow-left')
+
+        isLeft ? corrente_itens_topFilme -= 5: corrente_itens_topFilme += 5
+
+        if (corrente_itens_topFilme >= max_itens_topFilme) {
+            corrente_itens_topFilme = 0
+        }
+
+        if ( corrente_itens_topFilme < 0) {
+            corrente_itens_topFilme = max_itens_topFilme - 1
+        }
+
+        itens_topFilme[corrente_itens_topFilme].scrollIntoView({
+            inline: 'start',
+            behavior: "smooth",
+            block: "nearest"
+        })
+    })
+})
+
+// Slider apenas series policiais - abreviação - sp
+
+let corrente_itens_sp = 0
+
+const controle_sp = document.querySelectorAll('.control-sp')
+const itens_sp = document.querySelectorAll('.item-sp')
+const max_itens_sp = itens_sp.length
+
+controle_sp.forEach((control) => {
+    control.addEventListener("click", () => {
+        const isLeft = control.classList.contains('arrow-left')
+
+        isLeft ? corrente_itens_sp -= 5: corrente_itens_sp += 5
+
+        if (corrente_itens_sp >= max_itens_sp) {
+            corrente_itens_sp = 0
+        }
+
+        if ( corrente_itens_sp < 0) {
+            corrente_itens_sp = max_itens_sp - 1
+        }
+
+        itens_sp[corrente_itens_sp].scrollIntoView({
+            inline: 'start',
+            behavior: "smooth",
+            block: "nearest"
+        })
+    })
+})
+
+// Slider apenas lançamentos - abreviação - lan
+
+let corrente_itens_lan = 0
+
+const controle_lan = document.querySelectorAll('.control-lan')
+const itens_lan = document.querySelectorAll('.item-lan')
+const max_itens_lan = itens_lan.length
+
+controle_lan.forEach((control) => {
+    control.addEventListener("click", () => {
+        const isLeft = control.classList.contains('arrow-left')
+
+        isLeft ? corrente_itens_lan -= 5: corrente_itens_lan += 5
+
+        if (corrente_itens_lan >= max_itens_lan) {
+            corrente_itens_lan = 0
+        }
+
+        if ( corrente_itens_lan < 0) {
+            corrente_itens_lan = max_itens_lan - 1
+        }
+
+        itens_lan[corrente_itens_lan].scrollIntoView({
+            inline: 'start',
+            behavior: "smooth",
+            block: "nearest"
+        })
+    })
+})
