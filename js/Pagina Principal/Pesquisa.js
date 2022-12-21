3f37525e7d99effaa529293292833a130d17f57d
Richard Luan <richardluan084@gmail.com>
2022-12-21 15:29:16 -0300

    Mudando a imagem que aparece ao pesquisar

--- js/Pagina Principal/Pesquisa.js
+++ js/Pagina Principal/Pesquisa.js
@@ -6,9 +6,7 @@
 - Site: https://www.themoviedb.org/settings/api?language=pt-BR
 
 - Response para imagens = https://image.tmdb.org/t/p/w500
-- Response para pesquisar por filmes = https://api.themoviedb.org/3/search/movie?api_key={key}&query={filme}&language={lingua}
-- Response para pesquisar por series = https://api.themoviedb.org/3/search/multi?api_key={key}&query={Serie}&language={língua}
-- videos e mais infomarções = https://api.themoviedb.org/3/movie/{Id do filme/serie}?api_key={key}&append_to_response=videos
+- Response para pesquisar por midias = https://api.themoviedb.org/3/search/multi?api_key={key}&query={Serie}&language={língua}
 */
 
 document.addEventListener("keyup", function(e) {
@@ -18,6 +16,9 @@ document.addEventListener("keyup", function(e) {
 })
 
 async function Search () {
+
+    // Resetando o html 
+
     let images = document.querySelector('.imagens-pesquisa')
     let ul = document.getElementById('lista-nomes')
 
@@ -28,48 +29,46 @@ async function Search () {
     let language = 'PT-br'
     let search = document.getElementById("campo-texto").value
 
-    // Fazendo busca por filme - endpoint normal
+    // Fazendo busca por midias
 
-    let endpoint_movie = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${search}&language=${language}`
-    let response_movie = await fetch(endpoint_movie)
-    let bodyJson_filme = await response_movie.json()
-
-    let id_movie = bodyJson_filme.results[0].id
-
-    // Endpoint de filme com mais informações
-
-    let endpoint_movie_more = `https://api.themoviedb.org/3/movie/${id_movie}?api_key=${key}&append_to_response=videos`  
-    let response_movie_more = await fetch(endpoint_movie_more)
-    let bodyJson_filme_more = await response_movie_more.json()  
+    let endpoint_media = `https://api.themoviedb.org/3/search/multi?api_key=${key}&query=${search}&language=${language}`
+    let response_media = await fetch(endpoint_media)
+    let bodyJson_media = await response_media.json()
 
     // Trocando de main
 
     document.getElementById('catalogo-sliders').style.display = 'none'
     document.querySelector('.modal-pesquisa').style.display = 'flex'
 
-    // Carregando imagens relacionadas a pesquisa
+    // Carregando imagens relacionadas a pesquisa e textos relacionados
 
-    for (let i=0;i<bodyJson_filme.results.length;i++) {
-        let background_filme = bodyJson_filme.results[i].backdrop_path
+    for (let i=0;i<bodyJson_media.results.length;i++) {
+        if (bodyJson_media.results[i].vote_average >= 6.0) {
+            let background_media = bodyJson_media.results[i].poster_path
 
-        if (background_filme != null) {
-            let image = document.createElement('input')
-            image.type = 'image'
-            image.className = 'item-search'
-            image.src = `https://image.tmdb.org/t/p/w500${background_filme}`
-            images.append(image)
-        }
-    }
+            if (background_media != null) {
+                let image = document.createElement('input')
+                image.type = 'image'
+                image.className = 'item-search'
+                image.src = `https://image.tmdb.org/t/p/w500${background_media}`
+                images.append(image)
+            }
+            
+            let name_media = bodyJson_media.results[i].original_name
 
-    // Carregando textos relacionados
-
-    for (let i=0;i<5;i++) {
-        let nome_filme = bodyJson_filme.results[i].title
-
-        if (nome_filme != null) {
-            let li = document.createElement('li')
-            li.innerText = nome_filme
-            ul.append(li)
+            if (name_media != null && i < 10) {
+                let li = document.createElement('li')
+                li.innerText = name_media
+                ul.append(li)
+            }
         }
     }
 }
+
+/*
+ - Response para ver temporadas de serie e informações dos episodios = 
+   https://api.themoviedb.org/3/tv/{serie_id}/season/{numero_temporada}?api_key={chave}&language={lingua}
+
+ - Response do episodio separado = 
+   https://api.themoviedb.org/3/tv/{serie_id}/season/{numero_temporada}/episode/{numero_episodio}?api_key={chave}&language={lingua}
+*/
