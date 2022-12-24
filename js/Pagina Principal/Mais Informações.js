66ad28fb322e7715d6e59ba00c464dc4c3e73578
Richard Luan <richardluan084@gmail.com>
2022-12-24 18:49:46 -0300

    Adicionei informações sobre filmes/serie pela api

--- js/Pagina Principal/Mais Informações.js
+++ js/Pagina Principal/Mais Informações.js
@@ -28,7 +28,9 @@ async function Mais_Informações (id_media,media_type) {
     let JsonId = await response_id.json()
 
     let image = JsonId.backdrop_path
-    document.getElementById("preview-imagem").src = `https://image.tmdb.org/t/p/w500${image}`
+    document.getElementById("preview-imagem").src = `https://image.tmdb.org/t/p/w500${image}` // Adicionando imagem
+
+    // adicionando titulo ou nome
 
     if (media_type == 'movie'){
         document.getElementById("titulo-midia").innerText = JsonId.title
@@ -36,21 +38,185 @@ async function Mais_Informações (id_media,media_type) {
         document.getElementById("titulo-midia").innerText = JsonId.name
     }
 
-    document.querySelector(".infos-baixo").innerHTML = JsonId.overview
+    document.querySelector(".infos-baixo").innerHTML = JsonId.overview // Adicionando resumo sobre a midia
 
-    let temporada
-    JsonId.number_of_seasons == 1 ? temporada = 'Temporada': temporada = 'Temporadas'
+    let season
+    JsonId.number_of_seasons == 1 ? season = 'Temporada': season = 'Temporadas' // Mudando frase
+
+    // Adicionando quantidade de temporadas ou tempo de filme
 
     if (media_type == 'tv'){
-        document.getElementById("temporadas").innerText = `${JsonId.number_of_seasons} ${temporada}`
+        document.getElementById("temporadas").innerText = `${JsonId.number_of_seasons} ${season}`
     } else {
-        document.getElementById("temporadas").innerText = JsonId.runtime
+        let time_minutes = JsonId.runtime
+
+        let hours = parseInt(time_minutes / 60)
+        let minutes = time_minutes - hours*60
+        document.getElementById("temporadas").innerText = `${hours}h ${minutes}min`
     }
 
+    // Adicionando imagem da compania
+    
     let company = JsonId.production_companies[0].logo_path
-    document.getElementById("company").src = `https://image.tmdb.org/t/p/w500${company}`
+    let logo = document.getElementById("company")
 
-    document.getElementById("ultimo-ano-lançamento").innerText = JsonId.last_air_date
+    if (company != undefined) {    
+        logo.style.visibility = 'visible'
+        logo.src = `https://image.tmdb.org/t/p/w500${company}`
+    } else {
+        logo.style.visibility = 'hidden'
+    }
+
+    if (JsonId.last_air_date != undefined) {
+        document.getElementById("ultimo-ano-lançamento").innerText = JsonId.last_air_date.substring(0,4) // Adicionando ultima data de lançamento
+    }
+
+    // Adicionando restrição de idade
+
+    let endpoint_rating = `https://api.themoviedb.org/3/${media_type}/${id_media}/content_ratings?api_key=${key}&language=${language}`
+    let response_rating= await fetch(endpoint_rating)
+    let jsonRating = await response_rating.json()
+    
+    let age_response = await fetch("age_restriction.json")
+    let jsonAge = await age_response.json()
+
+    if (media_type == 'tv'){
+        for (let i=0;i<jsonRating.results.length;i++){
+            if (jsonRating.results[i].iso_3166_1 == "BR") {
+
+                let age = jsonRating.results[i].rating
+
+                for (let cont=0;cont<jsonAge.result.length;cont++){
+                    if (jsonAge.result[cont].age == age) {
+                        document.getElementById("avaliacao").style.backgroundColor = jsonAge.result[cont].background
+                        document.getElementById("numero-avaliacao").innerText = jsonAge.result[cont].age
+                    }
+                }
+            }
+        }
+    } else {
+        for (let i=0;i<JsonId.genres.length;i++){
+            if (JsonId.genres[i].name == 'Animação'){
+                document.getElementById("avaliacao").style.backgroundColor = 'blue'
+                document.getElementById("numero-avaliacao").innerText = 10
+            } else {
+                document.getElementById("avaliacao").style.backgroundColor = 'red'
+                document.getElementById("numero-avaliacao").innerText = 16
+            }
+        }
+    }
+
+    // Adicionando relevancia
+
+    let vote_average = JsonId.vote_average
+    let vote_min = 100 / vote_average
+    let relevance = parseInt(100 - vote_min)
+
+    let campo_relevancia = document.getElementById("relevancia")
+    campo_relevancia.innerText = `${relevance}% relevante`
+
+    if (relevance >= 70) {
+        campo_relevancia.style.color = '#46d369'
+    } else if ( relevance >= 50) {
+        campo_relevancia.style.color = '#d46102'
+    } else {
+        campo_relevancia.style.color = '#c40202'
+    }
+
+    // Adicionando elenco,generos e diretor
+    if (media_type == 'tv') {
+        let endpoint_elenco = `https://api.themoviedb.org/3/tv/${id_media}/season/1?api_key=${key}&language=${language}`
+        let response_elenco= await fetch(endpoint_elenco)
+        let jsonElenco = await response_elenco.json()
+
+        let coluna = document.querySelectorAll(".coluna-lista")
+        coluna[0].innerHTML = ''
+        coluna[1].innerHTML = ''
+        coluna[2].innerHTML = ''
+        
+        // Elenco
+
+        let spanElenco = document.createElement('span')
+        spanElenco.id = 'tag'
+        spanElenco.innerText = 'Elenco: '
+        coluna[0].append(spanElenco)
+
+        for(let i=0;i<jsonElenco.episodes[0].guest_stars.length;i++){
+            if (i<3){
+                let span = document.createElement('span')
+                span.id = 'item'
+
+                let a = document.createElement('a')
+                a.innerText = `${jsonElenco.episodes[0].guest_stars[i].original_name}, `
+
+                span.append(a)
+                coluna[0].append(span)
+            }
+            if (i>3){
+                let etc = document.createElement('p')
+                etc.innerText = 'etc...'
+                coluna[0].append(etc)
+                break
+            }
+        }
+        // Genero
+        let spanGenero = document.createElement('span')
+        spanGenero.id = 'tag'
+        spanGenero.innerText = 'Gênero: '
+        coluna[1].append(spanGenero)
+
+        for (let i=0;i<JsonId.genres.length;i++){
+            let span = document.createElement('span')
+            span.id = 'item'
+
+            let a = document.createElement('a')
+            a.innerText = `${JsonId.genres[i].name}, `
+
+            span.append(a)
+            coluna[1].append(span)
+        }
+        //Diretor
+        let spanDirector = document.createElement('span')
+        spanDirector.id = 'tag'
+        spanDirector.innerText = 'Diretor: '
+        coluna[2].append(spanDirector)
+
+        for (let i=0;i<jsonElenco.episodes[0].crew.length;i++){
+            if (jsonElenco.episodes[0].crew[i].job == 'Director') {
+                let span = document.createElement('span')
+                span.id = 'item'
+
+                let a = document.createElement('a')
+                a.innerText = `${jsonElenco.episodes[0].crew[i].original_name}.`
+
+                span.append(a)
+                coluna[2].append(span)
+            }
+        }
+    } else {
+        // Colocando genero do filme (unica coisa que dá)
+
+        let coluna = document.querySelectorAll(".coluna-lista")
+        coluna[0].innerHTML = ''
+        coluna[1].innerHTML = ''
+        coluna[2].innerHTML = ''
+
+        let spanGenero = document.createElement('span')
+        spanGenero.id = 'tag'
+        spanGenero.innerText = 'Gênero: '
+        coluna[0].append(spanGenero)
+
+        for (let i=0;i<JsonId.genres.length;i++){
+            let span = document.createElement('span')
+            span.id = 'item'
+
+            let a = document.createElement('a')
+            a.innerText = `${JsonId.genres[i].name}, `
+
+            span.append(a)
+            coluna[0].append(span)
+        }
+    }
 }
 
 const closeButton = document.getElementById("close-modal")
